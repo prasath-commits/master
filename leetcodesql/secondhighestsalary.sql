e739f32375644b37a8b86fa277518ae78cb48851
prasathsakthivel16@gmail.com <prasathsakthivel16@gmail.com>
2024-06-27 18:10:00 +0530

    some init msg

---
+++ leetcodesql/secondhighestsalary.sql
@@ -0,0 +1,43 @@
+-- Second Highest Salary
+-- https://leetcode.com/problems/second-highest-salary/
+
+/*
+
+Write a SQL query to get the second highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+For example, given the above Employee table, the query should return 200 as the second highest salary. If there is no second highest salary, then the query should return null.
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+
+*/
+
+# Solution1 
+select ifnull(max(Salary),0) AS SecondHighestSalary from Employee a 
+where a.Salary < (select max(Salary) from Employee b )
+
+# Solution2 
+SELECT ifnull(a.salary, 0) AS SecondHighestSalary
+FROM Employee a
+WHERE a.salary <
+    (SELECT max(b.salary)
+     FROM Employee b)
+ORDER BY 1 DESC
+LIMIT 1
+
+# Solution3
+# Time:  O(n)
+# Space: O(1)
+SELECT (SELECT MAX(Salary) FROM Employee WHERE Salary NOT IN (SELECT MAX(Salary) FROM Employee)) SecondHighestSalary;
+# or
+SELECT (SELECT Salary FROM Employee GROUP BY Salary ORDER BY Salary DESC LIMIT 1,1) SecondHighestSalary;
